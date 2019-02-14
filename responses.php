7bfb4d1d583eaf9ace4e958df4ecdef7e402a7ff
Lynh Tran <jenky.w0w@gmail.com>
2019-02-14 14:28:54 +0700

    Update response examples

--- responses.php
+++ responses.php
@@ -76,31 +76,49 @@
  * @OA\Response(
  *   response="BadRequest",
  *   description="Bad Request",
- *   @OA\JsonContent(ref="#/components/schemas/ErrorResponse"),
+ *   @OA\JsonContent(
+ *     ref="#/components/schemas/ErrorResponse",
+ *     example={"message": "Bad Request", "type": "HttpException", "status_code": 400},
+ *   ),
  * ),
  * @OA\Response(
  *   response="Unauthenticated",
  *   description="Unauthenticated",
- *   @OA\JsonContent(ref="#/components/schemas/ErrorResponse"),
+ *   @OA\JsonContent(
+ *     ref="#/components/schemas/ErrorResponse",
+ *     example={"message": "Unauthenticated", "type": "AuthenticationException", "status_code": 401},
+ *   ),
  * ),
  * @OA\Response(
  *   response="Forbidden",
  *   description="Access forbidden",
- *   @OA\JsonContent(ref="#/components/schemas/ErrorResponse"),
+ *   @OA\JsonContent(
+ *     ref="#/components/schemas/ErrorResponse",
+ *     example={"message": "Forbidden", "type": "HttpException", "status_code": 403},
+ *   ),
  * ),
  * @OA\Response(
  *   response="NotFound",
  *   description="Resource was not found",
- *   @OA\JsonContent(ref="#/components/schemas/ErrorResponse"),
+ *   @OA\JsonContent(
+ *     ref="#/components/schemas/ErrorResponse",
+ *     example={"message": "Not Found", "type": "HttpException", "status_code": 404},
+ *   ),
  * ),
  * @OA\Response(
  *   response="Unprocessable",
  *   description="The given data was invalid",
- *   @OA\JsonContent(ref="#/components/schemas/ValidationErrorResponse"),
+ *   @OA\JsonContent(
+ *     ref="#/components/schemas/ValidationErrorResponse",
+ *     example={"message": "The given data was invalid.", "type": "ValidationException", "status_code": 422, "errors": {"type": {"The type field is required."}, "reason": {"The reason field is required."}}},
+ *   ),
  * ),
  * @OA\Response(
  *   response="ServerError",
  *   description="Internal server error",
- *   @OA\JsonContent(ref="#/components/schemas/ErrorResponse"),
+ *   @OA\JsonContent(
+ *     ref="#/components/schemas/ErrorResponse",
+ *     example={"message": "Internal server error", "type": "HttpException", "status_code": 500},
+ *   ),
  * ),
  */
